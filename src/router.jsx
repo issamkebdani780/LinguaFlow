a08fb45c9b63a6392562f35e074629b78dbcb899
Issam Kebdani <kebdaniissam780@gmail.com>
2025-12-22 13:59:28 +0100

    modification in router

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Signin from "./components/signin";
 import Signup from "./components/signup";
-import Dashboard from "./section/Dashboard.jsx";
+import Dashboard from "./Dashboard/Dashboard.jsx";
 import App from "./App.jsx";
 import PrivateRouter from "./PrivateRouter.jsx";
 
